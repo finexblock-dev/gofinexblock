e71d0d401ff69e58ba0218033c429c88f4a06aa9
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-07-05 02:50:17 +0900

    fix: fix acquire lock

--- finexblock/trade/lock.go
+++ finexblock/trade/lock.go
@@ -2,13 +2,14 @@ package trade
 
 import (
 	"context"
+	"time"
 )
 
 func (a *tradeService) AcquireLock(ctx context.Context, uuid, currency string) (bool, error) {
 	var key string
 
 	key = getAccountLockKey(uuid, currency)
-	return a.redisClient.SetNX(ctx, key, lock, 10).Result()
+	return a.redisClient.SetNX(ctx, key, lock, time.Second*10).Result()
 }
 
 func (a *tradeService) ReleaseLock(ctx context.Context, uuid, currency string) error {
