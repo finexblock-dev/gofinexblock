d63626bcb235b681fce883eb53f24034fd88653a
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-07-04 23:14:38 +0900

    fix: update Snapshot service

--- finexblock/order/snapshot.go
+++ finexblock/order/snapshot.go
@@ -9,15 +9,6 @@ import (
 )
 
 func (o *orderService) Snapshot(tx *gorm.DB, symbolID uint, bids []*grpc_order.Order, asks []*grpc_order.Order) error {
-	var symbol string
-	switch {
-	case len(asks) != 0:
-		symbol = asks[0].Symbol.String()
-	case len(bids) != 0:
-		symbol = bids[0].Symbol.String()
-	default:
-		return fmt.Errorf("there is no order")
-	}
 
 	askOrderList, err := json.Marshal(asks)
 	if err != nil {
@@ -29,15 +20,11 @@ func (o *orderService) Snapshot(tx *gorm.DB, symbolID uint, bids []*grpc_order.O
 		return fmt.Errorf("failed to marshal order list: [%v]", err)
 	}
 
-	var orderSymbol *order.OrderSymbol
-	if err := o.db.Table(orderSymbol.TableName()).Where("name = ?", symbol).First(&orderSymbol).Error; err != nil {
-		return fmt.Errorf("failed to get order symbol: [%v]", err)
+	var _snapShotOrderBook = &order.SnapshotOrderBook{
+		OrderSymbolID: symbolID,
+		BidOrderList:  string(bidOrderList),
+		AskOrderList:  string(askOrderList),
 	}
-
-	var _snapShotOrderBook *order.SnapshotOrderBook
-	_snapShotOrderBook.OrderSymbolID = orderSymbol.ID
-	_snapShotOrderBook.AskOrderList = string(askOrderList)
-	_snapShotOrderBook.BidOrderList = string(bidOrderList)
 	if err := o.db.Table(_snapShotOrderBook.TableName()).Create(_snapShotOrderBook).Error; err != nil {
 		return fmt.Errorf("failed to create snapshot order book: [%v]", err)
 	}
