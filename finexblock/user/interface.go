5dfb7072d68a79f332fa53befa5967fdd91750e6
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-07-05 17:38:54 +0900

    add: add user service, imple types.Service interface

--- finexblock/user/interface.go
+++ finexblock/user/interface.go
@@ -1,11 +1,15 @@
 package user
 
 import (
+	context "context"
+	"database/sql"
 	"github.com/finexblock-dev/gofinexblock/finexblock/entity/user"
+	"github.com/finexblock-dev/gofinexblock/finexblock/types"
 	"gorm.io/gorm"
 )
 
 type Service interface {
+	types.Service
 	FindUserByUUID(tx *gorm.DB, uuid string) (*user.User, error)
 	FindUserByUUIDs(tx *gorm.DB, uuids []string) ([]*user.User, error)
 	FindUserByID(tx *gorm.DB, id uint) (*user.User, error)
@@ -16,6 +20,22 @@ type Service interface {
 
 type userService struct {
 	db *gorm.DB
+}
+
+func (u *userService) Ctx() context.Context {
+	return context.Background()
+}
+
+func (u *userService) CtxWithCancel(ctx context.Context) (context.Context, context.CancelFunc) {
+	return context.WithCancel(ctx)
+}
+
+func (u *userService) Tx(level sql.IsolationLevel) *gorm.DB {
+	return u.db.Begin(&sql.TxOptions{Isolation: level})
+}
+
+func (u *userService) Conn() *gorm.DB {
+	return u.db
 }
 
 func newUserService(db *gorm.DB) *userService {
