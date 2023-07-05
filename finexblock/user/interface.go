cfada2e1fd638b04827f0fe9d3e3d7435113a6a5
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-07-05 17:38:42 +0900

    add: add user service

--- finexblock/user/interface.go
+++ finexblock/user/interface.go
@@ -1,1 +1,27 @@
 package user
+
+import (
+	"github.com/finexblock-dev/gofinexblock/finexblock/entity/user"
+	"gorm.io/gorm"
+)
+
+type Service interface {
+	FindUserByUUID(tx *gorm.DB, uuid string) (*user.User, error)
+	FindUserByUUIDs(tx *gorm.DB, uuids []string) ([]*user.User, error)
+	FindUserByID(tx *gorm.DB, id uint) (*user.User, error)
+
+	BlockUser(tx *gorm.DB, id uint) error
+	UnBlockUser(tx *gorm.DB, id uint) error
+}
+
+type userService struct {
+	db *gorm.DB
+}
+
+func newUserService(db *gorm.DB) *userService {
+	return &userService{db: db}
+}
+
+func NewService(db *gorm.DB) Service {
+	return newUserService(db)
+}
