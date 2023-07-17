d0a2926554857ec8570eea6cdf60e268bc3c2938
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-07-18 03:06:26 +0900

    add: add placement package

---
+++ finexblock/placement/placement.interface.go
@@ -0,0 +1,14 @@
+package placement
+
+import (
+	"github.com/finexblock-dev/gofinexblock/finexblock/event"
+	"github.com/finexblock-dev/gofinexblock/finexblock/safety"
+	"github.com/finexblock-dev/gofinexblock/finexblock/stream"
+)
+
+type Engine interface {
+	safety.Subscriber
+	stream.Consumer
+	stream.Claimer
+	event.Hook
+}
