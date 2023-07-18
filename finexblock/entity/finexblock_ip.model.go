2dff7bb797c222a5174a33aca66a43a5d32878c6
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-07-19 01:29:21 +0900

    fix: fix FinexblockServerIP model

--- finexblock/entity/finexblock_ip.model.go
+++ finexblock/entity/finexblock_ip.model.go
@@ -1,12 +1,9 @@
 package entity
 
-import "gorm.io/gorm"
-
 type FinexblockServerIP struct {
 	ID       uint   `gorm:"column:id;primary_key;auto_increment;comment:'기본키'"`
 	ServerID uint   `gorm:"column:server_id;not null;comment:'서버 id'"`
 	IP       string `gorm:"column:ip;type:longtext;not null;comment:'IP 주소'"`
-	gorm.Model
 }
 
 // TableName sets the insert table name for this struct type
