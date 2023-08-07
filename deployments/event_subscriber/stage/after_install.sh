58be72d5898ed682599015de620fd266c847193f
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-08-08 02:08:56 +0900

    fix: fix event subscriber deployments

--- deployments/event_subscriber/stage/after_install.sh
+++ deployments/event_subscriber/stage/after_install.sh
@@ -5,5 +5,5 @@ docker rm -f $(docker ps -aq)
 docker rmi $(docker images | grep -v 'golang' | awk '{if(NR>1) print $3}')
 sudo systemctl start docker
 
-docker build -t finexblock-event-subscriber -f build/Dockerfile .
+docker build -t finexblock-event-subscriber -f build/event-subscriber.dockerfile .
 docker run -d --network=host finexblock-event-subscriber:latest
